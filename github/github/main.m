1dc861d707e19fc66041aac605b59d1cef1fb5a7
DannyWong <105192351@qq.com>
2015-11-09 10:07:14 +0800

    搭建框架

---
+++ github/github/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  github
+//
+//  Created by 黄道坚 on 15/11/9.
+//  Copyright © 2015年 lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
